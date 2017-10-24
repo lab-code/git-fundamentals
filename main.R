b18b22974216aa39abd7626aed45d35700726c58
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-10-23 20:47:48 -0700

    Adds link to tasks

--- main.R
+++ main.R
@@ -1,2 +1,4 @@
+#https://github.com/lab-code/R-for-non-programmers/blob/master/R/Practice/data-frames.md
+
 df_bob <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/bob-ross/elements-by-episode.csv")
 summary(df_bob)
