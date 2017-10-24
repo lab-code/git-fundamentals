8c8829275777495b9345f4920addccac76093b89
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-10-23 20:53:01 -0700

    Solves 5th task

--- main.R
+++ main.R
@@ -13,3 +13,8 @@ sum(df_bob$WINTER)/n_episodes
 
 # 4
 nrow(df_bob[df_bob$TREES == 1 & df_bob$WINTER == 1, ])/n_episodes
+
+# 5
+#F First two columns are strings. So we want to add only numbers in the third to the last one c(3:70)
+df_bob$N_THEMES = rowSums(df_bob[, c(3:70)])
+summary(df_bob$N_THEMES)
